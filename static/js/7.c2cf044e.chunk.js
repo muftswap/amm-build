e2a09f309f4520baa21cc981b0f1550fac347280
Shahzeb Khan <shahzeb8285@gmail.com>
2021-12-08 01:35:36 +0530

    first commit

---
+++ static/js/7.c2cf044e.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoose-frontend-amm"]=this["webpackJsonpgoose-frontend-amm"]||[]).push([[7],{627:function(n,o){},628:function(n,o){},637:function(n,o){},638:function(n,o){},657:function(n,o){},659:function(n,o){},662:function(n,o){},663:function(n,o){},668:function(n,o){},672:function(n,o){},721:function(n,o){}}]);
+//# sourceMappingURL=7.c2cf044e.chunk.js.map
---
+++ static/js/8.54be6a0d.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoose-frontend-amm"]=this["webpackJsonpgoose-frontend-amm"]||[]).push([[8],{627:function(n,o){},628:function(n,o){},637:function(n,o){},638:function(n,o){},657:function(n,o){},659:function(n,o){},662:function(n,o){},663:function(n,o){},668:function(n,o){},672:function(n,o){},972:function(n,o){}}]);
+//# sourceMappingURL=8.54be6a0d.chunk.js.map
